4a03a4685e7ccc2c8e72ef2ac77b330467d14467
Hamza11067 <hamzakhalid1067@gmail.com>
2025-09-06 11:40:25 +0500

    Added react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <div className="text-3xl text-red-400">Hello World!</div>
+      <BrowserRouter basename="/"> 
+        <Routes>
+          <Route path="/" element={<Layout />} >
+            <Route index element={<div>Home Page</div>} />
+            <Route path="about" element={<div>About Page</div>} />
+            <Route path="contact" element={<div>Contact Page</div>} />
+            <Route path="*" element={<div>404 Not Found</div>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
